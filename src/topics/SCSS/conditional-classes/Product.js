b2436c357a8225a22841b72edc6bbec850edb3bd
Stan <stanip@hotmail.de>
2020-05-24 18:37:05 +0200

    a

--- src/topics/SCSS/conditional-classes/Product.js
+++ src/topics/SCSS/conditional-classes/Product.js
@@ -1,31 +1,53 @@
 import React from 'react';
 import './product.scss'
 
+const getPriceClass = (price) => {
+  if (price === 100) {
+    return '--cheap'
+  }
+  if (price < 100) {
+    return '--super-cheap'
+  }
+  if (price >= 0 && price <= 10) {
+    return '--basically-for-free'
+  }
+  /*
+  * other classes go here (ノ-_-)ノ~┻━┻
+  */
+  return '--super-mega-cheap'
+}
+
+const AnotherNestedComponent = ({price, className}) => {
+  return (
+    <div>
+      <div className={`someClasses ${className}`}>
+        {price}
+      </div>
+    </div>
+  )
+}
+
+const ProductNestedComponent = ({price, className}) => {
+  return (
+    <div>
+      <div>
+        <AnotherNestedComponent price={price} className={className}/>
+      </div>
+    </div>
+  )
+}
+
 const Product = ({title, subtitle, description, price, variant, color, size, available}) => {
   console.log(price)
-
-  const getPriceClass = () => {
-    if (price === 100) {
-      return '--cheap'
-    }
-    if (price < 100) {
-      return '--super-cheap'
-    }
-    if (price >= 0 && price <= 10) {
-      return '--basically-for-free'
-    }
-    /*
-    * other classes go here (ノ-_-)ノ~┻━┻
-    */
-    return '--super-mega-cheap'
-  }
 
   return (
     <div className={`product product--${available ? 'available' : 'unavailable'}`}>
       <div className={'product__title'}>{title}</div>
       <div className={'product__subtitle'}>{subtitle}</div>
       <div className={'product__description'}>{description}</div>
-      <div className={`product__price product__price${getPriceClass()}`}>{price}</div>
+      <div>
+        <ProductNestedComponent price={price} className={`product__price product__price${getPriceClass()}`}/>
+      </div>
       <div className={`product__color product__color--${color}`}>{color}</div>
       <div className={'product__size'}>{size}</div>
     </div>
